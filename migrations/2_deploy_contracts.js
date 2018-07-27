f32f5aac626f46a4acd5966ac51ba05f05083af9
Phil Elsasser <phil@marketprotocol.io>
2018-07-27 11:26:06 -0600

    update to new version of marketprotocol@1.3.0

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -117,7 +117,7 @@ module.exports = function(deployer, network) {
                               .contractAddress;
                           return collateralPoolFactory.deployMarketCollateralPool(
                               marketContractDeployedAddress,
-                              { gas: 1800000 }
+                              { gas: 1900000 }
                             );
                         });
                     });
