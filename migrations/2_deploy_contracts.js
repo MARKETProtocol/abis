69f780ea30e2cafe10bf7882fab286cd10c2bc57
Phil Elsasser <phil@marketprotocol.io>
2018-10-15 20:53:55 -0600

    updated migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,17 +4,14 @@ const MathLib = artifacts.require(
 const OrderLib = artifacts.require(
   '@marketprotocol/marketprotocol/OrderLib.sol'
 );
-const QueryTest = artifacts.require(
-  '@marketprotocol/marketprotocol/OraclizeQueryTest.sol'
-);
 const MarketContractOraclize = artifacts.require(
   '@marketprotocol/marketprotocol/MarketContractOraclize.sol'
 );
 const MarketContractFactory = artifacts.require(
   '@marketprotocol/marketprotocol/MarketContractFactoryOraclize.sol'
 );
-const MarketCollateralPoolFactory = artifacts.require(
-  '@marketprotocol/marketprotocol/MarketCollateralPoolFactory.sol'
+const MarketCollateralPool = artifacts.require(
+  '@marketprotocol/marketprotocol/MarketCollateralPool.sol'
 );
 const MarketContractRegistry = artifacts.require(
   '@marketprotocol/marketprotocol/MarketContractRegistry.sol'
@@ -22,74 +19,67 @@ const MarketContractRegistry = artifacts.require(
 const MarketToken = artifacts.require(
   '@marketprotocol/marketprotocol/MarketToken.sol'
 );
+const MarketTradingHub = artifacts.require(
+  '@marketprotocol/marketprotocol/MarketTradingHub.sol'
+);
 
-module.exports = function(deployer, network) {
+module.exports = function (deployer, network) {
   if (network !== 'live') {
-    deployer.deploy(MathLib);
-    deployer.deploy(OrderLib);
-    deployer.deploy(MarketContractRegistry).then(function() {
+    return deployer.deploy([MathLib, OrderLib, MarketContractRegistry]).then(function () {
 
       deployer.link(
         MathLib,
-        QueryTest,
-        MarketContractOraclize,
-        MarketContractFactory
+        [
+          MarketContractOraclize,
+          MarketCollateralPool,
+          MarketContractFactory
+        ]
       );
 
-      deployer.link(
-        MathLib,
-        MarketCollateralPoolFactory
-      );
+      deployer.link(OrderLib, [MarketContractFactory, MarketContractOraclize, MarketTradingHub]);
 
-      deployer.link(OrderLib, MarketContractFactory, MarketContractOraclize);
-      // deploy our quest test contract
-      deployer.deploy(QueryTest).then(function(queryTestInstance) {
-        // by forcing the first query when we deploy we can make sure all the prices are accurate since
-        // the first query is free with oraclize.
-        return queryTestInstance.testOracleQuery(
-          'URL',
-          'json(https://api.kraken.com/0/public/Ticker?pair=BCHUSD).result.BCHUSD.c.0',
-        );
-      });
+      // deploy the global collateral pool
+      return deployer.deploy(MarketCollateralPool).then(function (marketCollateralPool) {
 
-      // deploy MKT token
-      const marketTokenToLockForTrading = 0; // for testing purposes, require no lock
-      const marketTokenAmountForContractCreation = 0; //for testing purposes require no balance
-      return deployer
-        .deploy(
-          MarketToken,
-          marketTokenToLockForTrading,
-          marketTokenAmountForContractCreation
-        )
-        .then(function() {
-          // deploy and set up main factory to create MARKET Protocol smart contracts.
-          return MarketContractRegistry.deployed().then(function(
-            marketContractRegistry
-          ) {
-
+        // deploy MKT token
+        const marketTokenToLockForTrading = 0; // for testing purposes, require no lock
+        const marketTokenAmountForContractCreation = 0; //for testing purposes require no balance
+        return deployer
+          .deploy(
+            MarketToken,
+            marketTokenToLockForTrading,
+            marketTokenAmountForContractCreation
+          )
+          .then(function () {
+            // deploy our trading hub!
             return deployer.deploy(
-              MarketCollateralPoolFactory,
-              marketContractRegistry.address
-            ).then(function(collateralPoolFactory)
-            {
-              return deployer
-                .deploy(
-                  MarketContractFactory,
-                  marketContractRegistry.address,
-                  MarketToken.address,
-                  collateralPoolFactory.address,
-                  {
-                    gas: 7000000
-                  }
-                )
-                .then(function(factory) {
-                  // white list the factory
-                  return marketContractRegistry
-                    .addFactoryAddress(factory.address)
+              MarketTradingHub,
+              MarketToken.address,
+              MarketCollateralPool.address
+            ).then(function (marketTradingHub) {
+              return marketCollateralPool.setMarketTradingHubAddress(
+                marketTradingHub.address
+              ).then(function () {
+                // deploy and set up main factory to create MARKET Protocol smart contracts.
+                return MarketContractRegistry.deployed().then(function (
+                  marketContractRegistry
+                ) {
+                  return deployer.deploy(
+                    MarketContractFactory,
+                    marketContractRegistry.address,
+                    MarketToken.address,
+                    {
+                      gas: 7000000
+                    }
+                  ).then(function (factory) {
+                      // white list the factory
+                      return marketContractRegistry.addFactoryAddress(factory.address)
+                  });
                 });
+              });
             });
           });
-        });
+      });
     });
   }
 };
