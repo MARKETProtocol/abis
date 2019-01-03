27ca04efabbfcfa30cd0b10e18899c71ec3faa29
Phil Elsasser <phil@marketprotocol.io>
2019-01-03 07:52:53 -0700

    updated abis

--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const privateKey = ""; // used for testnets only!
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -8,9 +11,10 @@ module.exports = {
       network_id: '*' // Match any network id
     },
     kovan: {
-      host: '127.0.0.1',
-      port: 8545,
-      from: '0x0088BD180722C48602770d355E692D2EBFbA86Ac',
+      provider: () => new HDWalletProvider(privateKey, "https://kovan.infura.io/v3/cd4068358ac743c082c994387eac4644"),
+      //host: '127.0.0.1',
+      //port: 8545,
+      //from: '0x0088BD180722C48602770d355E692D2EBFbA86Ac',
       network_id: 42,
       gasPrice: 1000000000 // Specified in Wei
     }
